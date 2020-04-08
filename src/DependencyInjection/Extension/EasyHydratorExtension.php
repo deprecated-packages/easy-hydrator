7e35d57387b00f625600e05d695db129e1d12e19
TomasVotruba <tomas.vot@gmail.com>
2020-04-09 01:23:20 +0200

    [SymfonyStaticDumper] Add bundle

--- src/DependencyInjection/Extension/EasyHydratorExtension.php
+++ src/DependencyInjection/Extension/EasyHydratorExtension.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Symplify\EasyHydrator\DependencyInjection;
+namespace Symplify\EasyHydrator\DependencyInjection\Extension;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
