f2b2095a873eb93b6d05d5e287f1e1a41d881be3
Tomas Votruba <tomas.vot@gmail.com>
2020-10-13 01:13:28 +0200

    [EasyHydrator] Make use rector split package (#2360)

---
+++ tests/HttpKernel/EasyHydratorTestKernel.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyHydrator\Tests\HttpKernel;
+
+use Rector\SimplePhpDocParser\Bundle\SimplePhpDocParserBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symplify\EasyHydrator\EasyHydratorBundle;
+use Symplify\SymplifyKernel\Bundle\SymplifyKernelBundle;
+use Symplify\SymplifyKernel\HttpKernel\AbstractSymplifyKernel;
+
+final class EasyHydratorTestKernel extends AbstractSymplifyKernel
+{
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        $loader->load(__DIR__ . '/../config.php');
+
+        parent::registerContainerConfiguration($loader);
+    }
+
+    /**
+     * @return BundleInterface[]
+     */
+    public function registerBundles(): iterable
+    {
+        return [new EasyHydratorBundle(), new SymplifyKernelBundle(), new SimplePhpDocParserBundle()];
+    }
+}
