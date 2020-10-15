a37d0da35dd0df1907d9e6d9a1bd634b986791f8
TomasVotruba <tomas.vot@gmail.com>
2020-10-15 15:38:51 +0200

    open 8.4-dev

---
+++ src/ArrayToValueObjectHydrator.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyHydrator;
+
+use Symfony\Contracts\Cache\CacheInterface;
+
+/**
+ * @see \Symplify\EasyHydrator\Tests\ArrayToValueObjectHydratorTest
+ */
+final class ArrayToValueObjectHydrator
+{
+    /**
+     * @var CacheInterface
+     */
+    private $cache;
+
+    /**
+     * @var ClassConstructorValuesResolver
+     */
+    private $classConstructorValuesResolver;
+
+    public function __construct(CacheInterface $cache, ClassConstructorValuesResolver $classConstructorValuesResolver)
+    {
+        $this->cache = $cache;
+        $this->classConstructorValuesResolver = $classConstructorValuesResolver;
+    }
+
+    /**
+     * @param mixed[] $data
+     */
+    public function hydrateArray(array $data, string $class): object
+    {
+        $arrayHash = md5(serialize($data) . $class);
+
+        return $this->cache->get($arrayHash, function () use ($class, $data) {
+            $resolveClassConstructorValues = $this->classConstructorValuesResolver->resolve($class, $data);
+
+            return new $class(...$resolveClassConstructorValues);
+        });
+    }
+
+    /**
+     * @param mixed[][] $datas
+     * @return object[]
+     */
+    public function hydrateArrays(array $datas, string $class): array
+    {
+        $valueObjects = [];
+        foreach ($datas as $data) {
+            $valueObjects[] = $this->hydrateArray($data, $class);
+        }
+
+        return $valueObjects;
+    }
+}
